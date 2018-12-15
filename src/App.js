f4d2219c4f0d22e96bb8d98e895cdc11d0d0aa96
Matthew Schupack <schu34@gmail.com>
2018-12-15 15:30:48 -0500

    head title injection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {
-  Component
-} from 'react';
+import React from 'react';
+import {Helmet} from 'react-helmet'
 import quizData from './quizData.json'
 import Quiz from './components/Quiz';
 import Error from './components/Error';
 import Page from './components/Page';
 import './App.scss';
 
-class App extends Component {
+
+class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -25,9 +25,10 @@ class App extends Component {
   renderInner(quizData){
 
     if (quizData) {
-
       return <Quiz {...quizData} / >
-    } else return <Error / >
+    } else {
+      return <Error / >
+    }
   }
 
   render() {
@@ -36,6 +37,9 @@ class App extends Component {
       quizData
     } = this.state;
     return <Page>
+      <Helmet>
+        <title>Quizly | {quizData ? quizData.title : "Quiz not found"}</title>
+      </Helmet>
       {this.renderInner(quizData)}
     </Page>
   }
