d6e0a6bf58a10229ea2dd1369424a4db763b3cb5
Matthew Schupack <schu34@gmail.com>
2018-12-15 22:48:06 -0500

    state and better styles

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,55 @@
-import React from 'react';
-import Question from './QuizQuestion'
-import '../styles/quiz.sass';
+import React from "react";
+import Question from "./QuizQuestion";
+import "../styles/quiz.sass";
 
-const Quiz = ({title, questions})=>( 
-<div>
-	<h1> {title}</h1>
-	<div>
-		{questions.map(q=><Question {...q} />)}
-	</div>
-</div> 
-)
+export default class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =[]; 
+  }
 
-export default Quiz;
+  answerHandler = (idx, answer )=> {
+    const i = parseInt
+    this.setState(state=>{
+      const newState = JSON.parse(JSON.stringify(state));
+      newState.questions[idx].answer = answer;
+      return newState;
+    });
+  }
+
+  getEstimate = (answers)=>{
+    console.log(answers)
+    let a = 0
+    const len = this.props.questions.length
+    for(var i =0; i<len ; i++){
+      const b = answers[i]
+      if(b === true) a+=100;
+      else if(b === false) return a+=0;
+      else a+=50;
+    }
+    console.log(a)
+    return Math.round((a/len*10))/10
+  }
+
+  render = () => {
+    const { title, questions } = this.props;
+    const  answers  = questions.map(a=>a.answer);
+    const { answerHandler } = this;
+
+    return (
+      <div>
+        <h1> {title} {this.getEstimate(answers)}%</h1>
+        <div>
+          {questions.map((q, i) => (
+            <Question
+              yesHandler={() => answerHandler(i, true)}
+              noHandler={() => answerHandler(i, false)}
+              answer={answers[i]}
+              {...q}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  };
+}
--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import classNames from 'classnames';
 import '../styles/question.sass'
 
-const Question = ({ questionText }) => <div className="q-container"> 
+const Question = ({ questionText, yesHandler, noHandler, answer }) => <div className="q-container"> 
 	<h2 className="q-text">{ questionText }</h2>
 	<div className="q-answer-container">
-		<button className="q-yes"> Yes </button>
-		<button className="q-no"> No </button>
+		<button className={classNames('q-yes', {clicked: answer === true })} onClick={yesHandler}> Yes </button>
+		<button className={classNames('q-no', {clicked: answer === false })} onClick={noHandler}> No </button>
 	</div>
 </div> 
 
-export default Question
+export default Question;
